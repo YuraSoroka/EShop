17c954473883eb4f3dcb67f93fe6e5d7de287545
Yurii Soroka <yurasoroka1999@gmail.com>
2024-08-18 16:01:37 +0300

    Added common project BuildingBlocks. Added mock Post endpoint for creating a product

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(
+    string Name, 
+    List<string> Category, 
+    string Description, 
+    string ImageFile, 
+    decimal Price)
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = request.Name,
+            Category = request.Category,
+            Description = request.Description,
+            ImageFile = request.ImageFile,
+            Price = request.Price
+        };
+
+        return Task.FromResult(new CreateProductResult(Guid.NewGuid()));
+    }
+}
